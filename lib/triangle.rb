abe021d42ce8e0ce304f5e400be6fa1807248865
Christopher Black <christopher.black.2209@googlemail.com>
2021-05-01 23:43:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,11 @@ class Triangle
   def kind
     if @side_1 == 0 #|| @side_2 <= 0 || @side_3 <= 0 
       #@side_1+@side_2<@side_3#||@side_2+@side_3<@side_1||@side_1+@side_3<@side_2
-      begin
+      #begin
         raise TriangleError
-      rescue TriangleError => error 
-        puts error.message
-      end 
+      #rescue TriangleError => error 
+      #  puts error.message
+      #end 
         
         
     
